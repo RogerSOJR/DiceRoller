c286602d43dc1dad9c71da62d169ee23bac9244e
RogerSOJR <rogerio.oliveira144@etec.sp.gov.br>
2022-03-21 21:50:48 -0300

    Dice Images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,18 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{rollDice()}
     }
     private fun  rollDice(){
-        Toast.makeText(this, "Button Clicked",
-            Toast.LENGTH_SHORT).show()
+        val randomInt=(1..6).random()
+        val diceImage: ImageView = findViewById(R.id.imgDice)
+
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
